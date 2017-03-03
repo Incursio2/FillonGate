f04034ce9dd919138e99bc5650fe44eae11f3cc1
Dorian C <Dorian.caup@y-nov.com>
2017-03-03 14:43:02 +0100

    Canon fire setup without bullet

--- canon.cpp
+++ canon.cpp
@@ -3,21 +3,39 @@
 #include <QGraphicsRectItem>
 #include <QGraphicsSceneMouseEvent>
 #include <QPointF>
+#include <QTimer>
 
 #include <QDebug>
 
-Canon::Canon(int sceneWidth, int sceneHeight)
+Canon::Canon(int sceneWidth, int sceneHeight, QGraphicsItem *parent)
 {
    setPixmap(QPixmap(":/sprites/sprites/canon.png"));
 
    m_canonOriginPoint = new QPointF(pixmap().size().width() / 2, pixmap().size().height());
    setPos(sceneWidth / 2 - pixmap().size().width() / 2, sceneHeight - pixmap().size().height());
+
+   m_timer = new QTimer(this);
+   m_timer->start(500);
+   Canon::Handle();
 }
 
 Canon::~Canon()
 {
     delete m_canonOriginPoint;
     m_canonOriginPoint = nullptr;
+
+    delete m_timer;
+    m_timer = nullptr;
+}
+
+void Canon::Handle()
+{
+    m_timer->connect(m_timer, &QTimer::timeout, this,  &Canon::Fire);
+}
+
+void Canon::Fire()
+{
+    qDebug() << "Fire ! " << endl;
 }
 
 void Canon::mouseMoveEvent(QGraphicsSceneMouseEvent * mouseEvent)
--- canon.h
+++ canon.h
@@ -1,26 +1,33 @@
 #ifndef CANON_H
 #define CANON_H
 
-class QGraphicsSceneMouseEvent;
-class QPointF;
-
+#include <QObject>
 #include <QGraphicsPixmapItem>
 
-class Canon : public QGraphicsPixmapItem
+class QGraphicsSceneMouseEvent;
+class QPointF;
+class QTimer;
+
+class Canon : public QObject, public QGraphicsPixmapItem
 {
+    Q_OBJECT
+
     public:
-        Canon(int sceneWidth, int sceneHeight);
+        Canon(int sceneWidth, int sceneHeight, QGraphicsItem * parent = 0);
         ~Canon();
 
-        QPointF *m_canonOriginPoint;
+    public slots:
+        void mouseMoveEvent(QGraphicsSceneMouseEvent *mouseEvent);
+        void mousePressEvent(QGraphicsSceneMouseEvent *mouseEvent);
+        void mouseReleaseEvent(QGraphicsSceneMouseEvent *mouseEvent);
 
-public slots:
-    void mouseMoveEvent(QGraphicsSceneMouseEvent *mouseEvent);
-    void mousePressEvent(QGraphicsSceneMouseEvent *mouseEvent);
-    void mouseReleaseEvent(QGraphicsSceneMouseEvent *mouseEvent);
+    private slots:
+         void Fire();
+    private:
+         QPointF *m_canonOriginPoint;
+         QTimer *m_timer;
 
-private:
-
+        void Handle();
 };
 
 #endif // CANON_H
